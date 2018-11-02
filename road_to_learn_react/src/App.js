5ef40491fdc3ea4773871670cc763e7fbf0b12f5
Mokaffe <903064667@qq.com>
2018-11-02 08:41:02 +0800

    add learn es6 part

--- road_to_learn_react/src/App.js
+++ road_to_learn_react/src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
 
-// ES5
-// function isSearched(searchTerm) {
-//   return function(item) {
-//     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-//   }
-// }
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -35,38 +19,81 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
     // 为了将onDismiss() 定义为类方法，需要在构造函数中绑定它。
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+
+
+  setSearchTopStories(result) {
+    console.log(result);
+    const {hits, page} = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ]
+
+    this.setState({
+      result: {hits: updatedHits, page}
+    });
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
   }
 
   onSearchChange(event) {
     this.setState({searchTerm: event.target.value});
   }
 
+  onSearchSubmit(event) {
+    const {searchTerm} = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
 
-  // 这是个在类中的功能函数，也在类里面，为什么还需要在构造函数中绑定？
   onDismiss(id) {
-    // eslint-disable-next-line
-    console.log(this);
-    const updateList = this.state.list.filter(item => item.objectID !== id);
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
     this.setState({
-      list: updateList
+      result: {...this.state.result, hits: updatedHits}
     });
   }
 
   render() {
-    const {list, searchTerm} = this.state;
+    const {result, searchTerm} = this.state;
+    if (!result) {
+      return null;
+    }
+    const page = (result && result.page) || 0;
     return (
-      <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange}>Search test</Search>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>Search</Search>
+        </div>
+        {result && <Table list={result.hits} onDismiss={this.onDismiss}/>}
+        <div className="interactions">
+          <button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>More</button>
+        </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    const {searchTerm} = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 }
 
@@ -74,18 +101,19 @@ class App extends React.Component {
 // 强制使用 props作为输入，JSX作为输出
 // 函数参数使用了解构方式
 // const { value, onChange, children } = props;
-const Search = ({value, onChange, children}) => {
+const Search = ({value, onChange, onSubmit, children}) => {
   return (
-    <form>
-      {children}<input type="text" value={value} onChange={onChange}></input>
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange}/>
+      <button type="submit">{children}</button>
     </form>
   );
 };
 
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({list, onDismiss}) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span><a href={item.url}>{item.title}</a></span>
         <span>{item.author}</span>
@@ -98,7 +126,7 @@ const Table = ({ list, pattern, onDismiss }) =>
     )}
   </div>;
 
-const Button = ({onClick, className='', children}) =>
+const Button = ({onClick, className = '', children}) =>
   <button onClick={onClick} className={className} type="button">{children}</button>
 
 export default App;
